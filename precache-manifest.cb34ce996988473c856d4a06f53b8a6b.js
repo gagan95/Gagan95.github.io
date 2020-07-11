a5d73a82ba44f648a9dd5d85cc17b3c87bda82c4
Gagan V <sam.gagan95@gmail.com>
2020-07-11 22:47:23 +0530

    react site

---
+++ precache-manifest.cb34ce996988473c856d4a06f53b8a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95131695f6ad8af199230ebab40c36a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9973bc431057faa7f0e0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de05b046696ffae69176",
+    "url": "/static/js/2.ba5ebd3c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ba5ebd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9973bc431057faa7f0e0",
+    "url": "/static/js/main.49360852.chunk.js"
+  },
+  {
+    "revision": "fae39dd47ad9a00bb02c",
+    "url": "/static/js/runtime-main.c24f648c.js"
+  }
+]);
